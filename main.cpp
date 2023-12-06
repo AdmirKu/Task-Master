e7df3bc91d8103ffba3e2abf812775d1a5ae528e
AdmirKu <148090272+AdmirKu@users.noreply.github.com>
2023-12-06 10:58:31 -0600

    User Authentication

--- main.cpp
+++ main.cpp
@@ -2,137 +2,145 @@
 #include <fstream>
 #include <vector>
 #include <algorithm>
+#include <sstream>
+#include <iomanip>
+#include <openssl/sha.h>
 
-class Task {
-public:
-    std::string description;
-    bool completed;
+// Helper function to hash a password using SHA-256
+std::string hashPassword(const std::string& password) {
+    unsigned char hash[SHA256_DIGEST_LENGTH];
+    SHA256_CTX sha256;
+    SHA256_Init(&sha256);
+    SHA256_Update(&sha256, password.c_str(), password.length());
+    SHA256_Final(hash, &sha256);
 
-    Task(const std::string& desc, bool comp = false) : description(desc), completed(comp) {}
-
-    // Convert task to a string for storage in a file
-    std::string toString() const {
-        return description + "," + (completed ? "1" : "0");
-    }
-};
+    std::stringstream ss;
+    for (int i = 0; i < SHA256_DIGEST_LENGTH; ++i) {
+        ss << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(
+return ss.str();
+}
 
 class User {
 public:
     std::string username;
+    std::string hashedPassword;
     std::vector<Task> tasks;
 
-    User(const std::string& name) : username(name) {}
-
-    void addTask(const std::string& desc) {
-        tasks.emplace_back(desc);
+    User(const std::string& name, const std::string& password) : username(name) {
+        // Hash the password before storing it
+        hashedPassword = hashPassword(password);
     }
 
-    void viewTasks() const {
-        std::cout << "Tasks for user " << username << ":\n";
-        for (const auto& task : tasks) {
-            std::cout << "- " << task.description << (task.completed ? " (Completed)" : "") << "\n";
-        }
+    // Validate the entered password against the stored hashed password
+    bool validatePassword(const std::string& enteredPassword) const {
+        return hashPassword(enteredPassword) == hashedPassword;
     }
 
-    void completeTask(const std::string& desc) {
-        auto it = std::find_if(tasks.begin(), tasks.end(), [&desc](const Task& task) {
-            return task.description == desc;
-        });
-
-        if (it != tasks.end()) {
-            it->completed = true;
-            std::cout << "Task '" << desc << "' marked as completed.\n";
-        } else {
-            std::cout << "Task not found.\n";
-        }
-    }
-
-    // Save user's tasks to a file
-    void saveTasksToFile(const std::string& filename) const {
-        std::ofstream file(filename);
-
-        if (file.is_open()) {
-            for (const auto& task : tasks) {
-                file << task.toString() << "\n";
-            }
-            std::cout << "Tasks saved to file.\n";
-        } else {
-            std::cerr << "Unable to open file for writing.\n";
-        }
-    }
-
-    // Load user's tasks from a file
-    void loadTasksFromFile(const std::string& filename) {
-        std::ifstream file(filename);
-
-        if (file.is_open()) {
-            tasks.clear(); // Clear existing tasks
-            std::string line;
-            while (std::getline(file, line)) {
-                // Split the line into description and completion status
-                size_t commaPos = line.find(',');
-                if (commaPos != std::string::npos && commaPos < line.length() - 1) {
-                    std::string desc = line.substr(0, commaPos);
-                    bool comp = line.substr(commaPos + 1) == "1";
-                    tasks.emplace_back(desc, comp);
-                }
-            }
-            std::cout << "Tasks loaded from file.\n";
-        } else {
-            std::cerr << "Unable to open file for reading.\n";
-        }
-    }
+    // Other member functions as before...
 };
 
+// Function to check if a user exists in the users file
+bool userExists(const std::string& username) {
+    std::ifstream file("users.txt");
+    std::string line;
+    while (std::getline(file, line)) {
+        if (line.substr(0, line.find(',')) == username) {
+            return true;
+        }
+    }
+    return false;
+}
+
+// Function to load a user's hashed password from the users file
+std::string loadUserPassword(const std::string& username) {
+    std::ifstream file("users.txt");
+    std::string line;
+    while (std::getline(file, line)) {
+        if (line.substr(0, line.find(',')) == username) {
+            return line.substr(line.find(',') + 1);
+        }
+    }
+    return "";
+}
+
+// Function to create a new user and store their credentials
+void createUser(const std::string& username, const std::string& password) {
+    std::ofstream file("users.txt", std::ios::app);
+    file << username << "," << hashPassword(password) << "\n";
+    std::cout << "User account created successfully.\n";
+}
+
+// Function to authenticate a user
+bool authenticateUser(User& user) {
+    std::string enteredUsername, enteredPassword;
+
+    std::cout << "Enter username: ";
+    std::cin >> enteredUsername;
+
+    // Check if the user exists
+    if (!userExists(enteredUsername)) {
+        std::cout << "User does not exist.\n";
+        return false;
+    }
+
+    std::cout << "Enter password: ";
+    std::cin >> enteredPassword;
+
+    // Load the hashed password for the entered username
+    std::string storedPassword = loadUserPassword(enteredUsername);
+
+    // Validate the entered password
+    if (user.validatePassword(enteredPassword)) {
+        std::cout << "Authentication successful. Welcome, " << enteredUsername << "!\n";
+        user.username = enteredUsername;
+        return true;
+    } else {
+        std::cout << "Incorrect password.\n";
+        return false;
+    }
+}
+
 int main() {
-    User currentUser("John Doe");
+    User currentUser("", "");  // Temporary user object to handle authentication
 
-    // Load tasks from a file
-    currentUser.loadTasksFromFile("tasks.txt");
-
-    // Menu-driven interface
     int choice;
     do {
-        std::cout << "\nTask Master - Welcome, " << currentUser.username << "!\n";
-        std::cout << "1. Add Task\n";
-        std::cout << "2. View Tasks\n";
-        std::cout << "3. Complete Task\n";
-        std::cout << "4. Save Tasks\n";
-        std::cout << "5. Exit\n";
+        std::cout << "\nTask Master - Welcome!\n";
+        std::cout << "1. Log In\n";
+        std::cout << "2. Create New User\n";
+        std::cout << "3. Exit\n";
         std::cout << "Enter your choice: ";
         std::cin >> choice;
 
         switch (choice) {
-        case 1: {
-            std::string taskDesc;
-            std::cout << "Enter task description: ";
-            std::cin.ignore(); // Clear input buffer
-            std::getline(std::cin, taskDesc);
-            currentUser.addTask(taskDesc);
-            break;
+            case 1:
+                if (authenticateUser(currentUser)) {
+                    // Load tasks for the authenticated user
+                    currentUser.loadTasksFromFile(currentUser.username + "_tasks.txt");
+                    // Continue with the main menu for task management
+                    // ...
+                }
+                break;
+            case 2: {
+                std::string newUsername, newPassword;
+                std::cout << "Enter new username: ";
+                std::cin >> newUsername;
+                if (!userExists(newUsername)) {
+                    std::cout << "Enter password: ";
+                    std::cin >> newPassword;
+                    createUser(newUsername, newPassword);
+                } else {
+                    std::cout << "Username already exists. Please choose a different username.\n";
+                }
+                break;
+            }
+            case 3:
+                std::cout << "Exiting Task Master. Goodbye!\n";
+                break;
+            default:
+                std::cout << "Invalid choice. Please try again.\n";
         }
-        case 2:
-            currentUser.viewTasks();
-            break;
-        case 3: {
-            std::string taskDesc;
-            std::cout << "Enter task description to mark as completed: ";
-            std::cin.ignore(); // Clear input buffer
-            std::getline(std::cin, taskDesc);
-            currentUser.completeTask(taskDesc);
-            break;
-        }
-        case 4:
-            // Save tasks to a file
-            currentUser.saveTasksToFile("tasks.txt");
-            break;
-        case 5:
-            std::cout << "Exiting Task Master. Goodbye!\n";
-            break;
-        default:
-            std::cout << "Invalid choice. Please try again.\n";
-        }
-    } while (choice != 5);
+    } while (choice != 3);
 
     return 0;
 }
